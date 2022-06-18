0117567ad4e23edeb4fa3ad0165fc15ce48972e3
Nishika-Khatri <nishikakhatri26@yahoo.com>
2022-06-18 16:07:21 +0530

    Day 5

--- C++/Pointers/Basics.cpp
+++ C++/Pointers/Basics.cpp
@@ -25,5 +25,16 @@ int main(){
     cout<<"New value of Ptr2: "<<*ptr2<<endl;
     cout<<"Address Stored in Ptr2 : "<<ptr2<<endl;
 
+    int a = 10;
+    int *pt = &a;
+
+    cout<<"Value of a befor increment : "<<a<<endl;
+    (*pt)++;
+    cout<<"Value of num after increment : "<<a<<endl;
+
+    //copying a pointer
+    int *pt2 = pt;
+    cout<<pt2<<"\t"<<pt<<endl;
+    cout<<*pt2<<"\t"<<*pt<<endl;
     return 0;
 }
