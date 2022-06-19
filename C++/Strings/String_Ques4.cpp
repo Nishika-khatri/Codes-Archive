df121e0d53b7676a9da4a4e53f6ece4e76a4a2bb
Nishika-Khatri <nishikakhatri26@yahoo.com>
2022-06-20 02:52:58 +0530

    Day 6

--- C++/Strings/String_Ques4.cpp
+++ C++/Strings/String_Ques4.cpp
@@ -1,41 +1,38 @@
 //Leetcode Question 3 Longest substring without repeating characters
 #include<iostream>
-#include<string>
+#include<set>
 
 using namespace std;
 
 int lengthOfLongestSubstring(string s){
 
-    int count=0,temp=0;
-    string str ="";
-    str.push_back(s[0]);
+    set<char> st;
+    int start=0,end=0;
+    int count=0;
 
-    for(int i=1;i<s.length();i++){
-        if(str.find(s[i])== -1){
-            str.push_back(s[i]);
+    while(start<s.length()){
+        auto it = st.find(s[start]);
+
+        if(it ==st.end()){
+            st.insert(s[start]);
+            if(start-end+1>count){
+                count = start-end +1;
+            }
+            start++;
         }
         else{
-            if(str.at(0)==s[i]){
-                str.erase(0);
-                str.push_back(s[i]);
-            }else{
-                str.erase(str.begin(),str.end());
-                str.push_back(s[i]);
-            }
-        }
-
-        temp = str.length();
-        if(temp>=count){
-            count=temp;
+            st.erase(s[end]);
+            end++;
         }
     }
+    
 
     return count;
 
 }
 
 int main(){
-    string s ="bbbbb";
+    string s ="abcabcbefighjkl";
 
     int result = lengthOfLongestSubstring(s);
     cout<<"The length of the Longest Substring is : "<<result<<endl;
