0796109667a863bd57df7bd3273337b7e292e321
Nishika-Khatri <nishikakhatri26@yahoo.com>
2022-06-25 22:05:23 +0530

    Day 10

--- C++/Recursion/SayDigits.cpp
+++ C++/Recursion/SayDigits.cpp
@@ -1,16 +1,14 @@
 #include<iostream>
 #include<string>
-#include<vector>
-#include<algorithm>
 
 using namespace std;
 
-void sayDigits(int num, string arr[],vector<string> &vec){
+void sayDigits(int num, string arr[]){
     if(num==0) return;
     int temp= num%10;
-    vec.push_back(arr[temp]);
     num=num/10;
-    sayDigits(num,arr,vec);
+    sayDigits(num,arr);
+    cout<<arr[temp]<<"\t";
 }
 
 int main(){
@@ -18,13 +16,7 @@ int main(){
     string arr[10]={"zero","one","two","three","four","five","six","seven","eight","nine"};
     cout<<"Enter any nummber : ";
     cin>>num;
-    vector<string> vec;
-    sayDigits(num,arr,vec);
-    reverse(vec.begin(),vec.end());
-
-    for(int i =0;i<vec.size();i++){
-        cout<<vec[i]<<"\t";
-    }
+    sayDigits(num,arr);
 
     return 0;
 }
