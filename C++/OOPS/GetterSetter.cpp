364f819d0f2306b47949b2d519def5a01065ce7f
Nishika-Khatri <nishikakhatri26@yahoo.com>
2022-07-01 19:04:33 +0530

    Day 13

--- C++/OOPS/GetterSetter.cpp
+++ C++/OOPS/GetterSetter.cpp
@@ -36,6 +36,7 @@ class Student{
 };
 
 int main(){
+    //Static Allocation 
     Student std1;
 
     int age;
@@ -50,6 +51,7 @@ int main(){
     cout<<"\nEnter Roll No. :";
     cin>>rollNo;
 
+
     std1.setName(str);
     std1.setAge(age);
     std1.setRollNo(rollNo);
@@ -57,6 +59,28 @@ int main(){
     cout<<"Student Name : "<<std1.getName()<<endl;
     cout<<"Student Age : "<<std1.getAge()<<endl;
     cout<<"Student Roll No. : "<<std1.getRollNo()<<endl;
-    
+
+
+    //Dynamic Allocation
+    Student *std2 = new Student;
+    //Method 1 
+    (*std2).setName("Varun");
+    (*std2).setAge(20);
+    (*std2).setRollNo(9377);
+
+    cout<<"Student Name : "<<(*std2).getName()<<endl;
+    cout<<"Student Age : "<<(*std2).getAge()<<endl;
+    cout<<"Student Roll No. : "<<(*std2).getRollNo()<<endl;
+
+    //Method 2
+    std2->setName("Ray");
+    std2->setAge(22);
+    std2->setRollNo(9391);
+
+    cout<<"Student Name : "<<std2->getName()<<endl;
+    cout<<"Student Age : "<<std2->getAge()<<endl;
+    cout<<"Student Roll No. : "<<std2->getRollNo()<<endl;
+
+
     return 0;
 }
