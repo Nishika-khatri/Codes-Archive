d5fbb82cf428e4315618694f19e2813424203983
Nishika-Khatri <nishikakhatri26@yahoo.com>
2022-07-22 15:13:08 +0530

    Day 16

--- C++/OOPS/CopyConstructor.cpp
+++ C++/OOPS/CopyConstructor.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<cstring>
 
 using namespace std;
 
@@ -7,27 +8,29 @@ class Patient{
     int age;
     int fee;
     public:
+    char *name;
+
     Patient(){
         cout<<"Welcome!"<<endl;
+        name = new char[100];
     }
-    // Patient(int age, int fee){
-    //     this->age=age;
-    //     this->fee=fee;
-    // }
-
-    // Patient(Patient& temp){
-    //     cout<<"\nCopy Constructor"<<endl;
-    //     this->age=temp.age;
-    //     this->fee=temp.fee;
-    // }
+    Patient(int age, int fee){
+        this->age=age;
+        this->fee=fee;
+    }
 
     Patient(Patient& temp){
         cout<<"\nCopy Constructor"<<endl;
-        this->age = temp->age;
-        this->fee = temp->fee;
+        char *ch = new char[strlen(temp.name)];
+        strcpy(ch,temp.name);
+        this->name = ch;
+        this->age = temp.age;
+        this->fee = temp.fee;
     }
 
     void print(){
+        cout<<"\n"<<endl;
+        cout<<"Patient's Name : "<<name<<endl;
         cout<<"Patient's Age : "<<age<<endl;
         cout<<"Fee charged to Patient : "<<fee<<endl;
     }
@@ -35,6 +38,14 @@ class Patient{
     void setVal(int age, int fee){
         this->age = age;
         this->fee = fee;
+    }
+
+    void setName(char name[]){
+        strcpy(this->name,name);
+    }
+
+    ~Patient(){
+        cout<<"Destructor Called"<<endl;
     }
 
     
@@ -47,8 +58,26 @@ int main(){
     //Copy constructor
     // Patient P2(P1);
     // P2.print();
+    char name[8]= "Nishika";
 
-    Patient *p1 = new Patient;
+    Patient *p1 = new Patient();
+    p1->setVal(30,1500);
+    p1->setName(name);
+    p1->print();
+
+    Patient p2(*p1);
+    cout<<"\nPatient 2 : "<<endl;
+    p2.print();
+
+    p1->name[0]='G';
+    cout<<"\nUpdated Info for P1 "<<endl;
+    p1->print();
+
+    cout<<"\nUpdated Info for P2 "<<endl;
+    p2.print();
+
+    delete p1;
+
 
 
     return 0;
